fb38e153246e9fe45af612e332e5157db2cbc176
Darius Bakunas-Milanowski <bakunas@gmail.com>
2018-03-31 17:34:04 -0400

    cleanup

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ from flask_socketio import SocketIO
 from pyvirt.resources.events import event_cb
 from pyvirt.utils.libvirt import LibvirtEventConnector
 
-eventLoopThread = None
-
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
