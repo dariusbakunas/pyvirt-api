387323c6fa96027c9ee85d0a1b990133f4ef1ad4
Darius Bakunas-Milanowski <bakunas@gmail.com>
2018-04-08 10:04:45 -0400

    add monkeypatch for eventlet

--- pyvirt/__init__.py
+++ pyvirt/__init__.py
@@ -8,10 +8,13 @@ from pyvirt.resources.domain import DomainList
 from pyvirt.resources.events import event_cb
 from pyvirt.utils.libvirt import LibvirtEventConnector
 
+import eventlet
+eventlet.monkey_patch()
+
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
-async_mode = None
+async_mode = 'eventlet'
 socketio = SocketIO()
 
 
